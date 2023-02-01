9417bd5a8b2678d6b89e13585f4fa39895325427
NolascoZapata <nolascozapata71@gmail.com>
2023-02-01 00:02:35 -0300

    reset-app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Header.css'
 function Header() {
   return (
-    <h1>WeatherApp <sup><i className='material-icons'>cloud_queue</i></sup></h1>
+    <a href="/"><h1>WeatherApp <sup><i className='material-icons'>cloud_queue</i></sup></h1> </a>
   )
 }
 
